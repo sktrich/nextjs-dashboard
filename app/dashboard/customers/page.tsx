57c33e49fb155602a52d6e21339c2808e533793e
sktrich <r.arumakan@gmail.com>
2024-01-09 01:18:17 -0300

    changes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1 className="text-xl text-amber-800">Customers Page</h1>;
+  }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,7 +13,10 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Customers', 
+  href: '/dashboard/customers', 
+  icon: UserGroupIcon 
+  },
 ];
 
 export default function NavLinks() {
@@ -25,7 +28,7 @@ export default function NavLinks() {
           <a
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-amber-50 hover:text-amber-700 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
